04469b49901fb89c03534197761aa747d0c7925a
wanda <wandaandita2003@gmail.com>
2023-11-23 00:54:51 +0700

    Update coin

---
+++ src/slashCommands/coin.ts
@@ -0,0 +1,51 @@
+import {SlashCommandBuilder, PermissionFlagsBits, CommandInteraction} from 'discord.js';
+import {SlashCommand} from '../types';
+import {item} from '../data/item';
+import {getGuildOption} from '../function';
+
+const command: SlashCommand = {
+	command: new SlashCommandBuilder()
+		.setName('coin')
+		.setDescription('Gửi lệnh GM thêm tiền tệ cho người chơi.')
+		.setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
+		.addStringOption((option) => option.setName('uid').setDescription('UID của người chơi').setRequired(true))
+		.addStringOption((option) =>
+			option
+				.setName('type')
+				.setDescription('Chọn loai tiền tệ')
+				.setRequired(true)
+				.addChoices(
+					{name: 'Đá sáng thế', value: 'mcoin'},
+					{name: 'Mora', value: 'scoin'},
+					{name: 'Nguyên thạch', value: 'hcoin'},
+					{name: 'Tiền Động Tiên', value: 'home_coin'}
+				)
+		)
+		.addNumberOption((option) => option.setName('amount').setDescription('Số lượng')),
+	cooldown: 1,
+	execute: async (interaction: CommandInteraction) => {
+		if (!interaction.isChatInputCommand()) return;
+		if (!interaction.guild) return interaction.reply('Không thể thực hiện ở DM');
+		const ip = await getGuildOption(interaction.guild, 'address');
+		const uid = interaction.options.getString('uid', true);
+		const type = interaction.options.getString('type', true);
+		const amount = interaction.options.getNumber('type') ?? 1;
+		const name = {
+			mcoin: 'Đá sáng thế',
+			scoin: 'Mora',
+			hcoin: 'Nguyên thạch',
+			home_coin: 'Tiền Động Tiên',
+		};
+		try {
+			await fetch(`http://${ip}:10106/api?region=dev_docker&ticket=GM&cmd=1116&uid=${uid}&msg=${type}%20${amount}`);
+			await interaction.reply({
+				content: `Đã thêm ${name[type as keyof typeof name]} cho người chơi UID ${uid}`,
+				ephemeral: true,
+			});
+		} catch (error) {
+			console.log(`Error in GM item: ${error.message}\nIP: ${ip}`);
+		}
+	},
+};
+
+export default command;
