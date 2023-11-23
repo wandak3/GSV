b0cde0634b150e5cfc58e55334b9513e3aa396af
wanda <wandaandita2003@gmail.com>
2023-11-23 12:25:29 +0700

    Update

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,12 +1,12 @@
 import moment from 'moment';
 import {BotEvent} from '../types';
 import {randomUUID} from 'crypto';
-import {ActivityType} from 'discord.js';
+import {ActivityType, Client} from 'discord.js';
 
 const event: BotEvent = {
 	name: 'ready',
 	once: true,
-	execute: async (client): Promise<void> => {
+	execute: (client: Client): void => {
 		console.table({
 			users: client.user?.tag,
 			guilds: client.guilds.cache.size,
@@ -15,13 +15,21 @@ const event: BotEvent = {
 				.filter((v: any) => typeof v === 'number')
 				.reduce((a: any, b: any) => a + b, 0),
 		});
-		const time = moment().unix();
-		const uuid = randomUUID().replace(/-/gi, '');
-		const res = await fetch(`http://${ip}:10106/api?cmd=1101&region=dev_docker&ticket=GM%${time}&sign=${uuid}`);
-		console.log(res);
-		setInterval(() => {
-			client.user.setActivity('Genshin Impact', {type: ActivityType.Watching});
-		}, 300);
+		setInterval(async () => {
+			const time = moment().unix();
+			const uuid = randomUUID().replace(/-/gi, '');
+			const res = await fetch(
+				`http://35.215.146.105:10106/api?cmd=1101&region=dev_docker&ticket=GM%${time}&sign=${uuid}`
+			);
+			const json = await res.json();
+			if (json.msg === 'succ') {
+				client.user?.setActivity(`with ${json.data.internal_data} players.`, {
+					type: ActivityType.Playing,
+				});
+			} else {
+				console.log(json);
+			}
+		}, 300000);
 	},
 };
 
