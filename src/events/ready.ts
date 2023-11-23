286d979ae92c8669c0272d404db2c90cc0a0da33
wanda <wandaandita2003@gmail.com>
2023-11-23 11:49:34 +0700

    Stopped tracking env.local, and env.staging

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,4 +1,7 @@
+import moment from 'moment';
 import {BotEvent} from '../types';
+import {randomUUID} from 'crypto';
+import {ActivityType} from 'discord.js';
 
 const event: BotEvent = {
 	name: 'ready',
@@ -12,6 +15,13 @@ const event: BotEvent = {
 				.filter((v: any) => typeof v === 'number')
 				.reduce((a: any, b: any) => a + b, 0),
 		});
+		const time = moment().unix();
+		const uuid = randomUUID().replace(/-/gi, '');
+		const res = await fetch(`http://${ip}:10106/api?cmd=1101&region=dev_docker&ticket=GM%${time}&sign=${uuid}`);
+		console.log(res);
+		setInterval(() => {
+			client.user.setActivity('Genshin Impact', {type: ActivityType.Watching});
+		}, 300);
 	},
 };
 
