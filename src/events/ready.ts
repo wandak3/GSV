d041466cb0e045fffc67718ec151ed1c134a7b5b
Your Name <you@example.com>
2024-04-21 04:15:26 +0700

    Fix formatting and add error handling in ready.ts

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,8 +1,8 @@
 import moment from 'moment';
 import { CronJob } from 'cron';
 import { getGachaScheduleConfig, getPlayerOnline } from '../function';
-import {BotEvent} from '../types';
-import {ActivityType, Client} from 'discord.js';
+import { BotEvent } from '../types';
+import { ActivityType, Client } from 'discord.js';
 import prisma from '../prisma/prisma';
 
 const event: BotEvent = {
@@ -16,17 +16,20 @@ const event: BotEvent = {
 		const job = new CronJob(
 			'* * 0 * * *', // cronTime
 			async function () {
-				const data = await getGachaScheduleConfig();
-				if (!data) return;
-				data.forEach(async (e) => {
-					const compare = moment(e.end_time).isBefore(moment());
-					if(e.enabled === 1 && compare) {
-						await prisma.t_gacha_schedule_config.update({
-							where: {schedule_id: e.schedule_id},
-							data: {enabled: 0},
-						});
-					}
-				});
+				try {
+					const data = await getGachaScheduleConfig();
+					if (!data) return;
+					data.forEach(async (e) => {
+						const compare = moment(e.end_time).isBefore(moment());
+						if (e.enabled === 1 && compare) {
+							await prisma.t_gacha_schedule_config.update({
+								where: { schedule_id: e.schedule_id },
+								data: { enabled: 0 },
+							});
+						}
+					});
+				} catch {
+				}
 			},
 			null, // onComplete
 			true, // start
@@ -34,16 +37,19 @@ const event: BotEvent = {
 		);
 		setInterval(async () => {
 			const onlinePlayer = await getPlayerOnline();
+			const start = Date.now();
+			await fetch('http://37.114.63.115:2888');
+			const ping = (Date.now() - start);
 			if (onlinePlayer === "down") {
 				client.user?.setActivity(`Bảo trì.`, {
 					type: ActivityType.Listening,
 				});
 			} else {
-				client.user?.setActivity(` với ${onlinePlayer} người.`, {
+				client.user?.setActivity(` với ${onlinePlayer} người. Ping ${ping}ms`, {
 					type: ActivityType.Playing,
 				});
 			}
-			
+
 		}, 60_000);
 	},
 };
